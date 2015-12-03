9a6b5bb01e5f2cf87710aa6992a76b024da673cd
mark <h091237557@gmail.com>
2015-12-03 17:55:39 +0800

    Update:change dir struct and add test

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  jshint = require('gulp-jshint'),
+  mocha = require('gulp-mocha');
+
+var testFiles = 'test/*.js';;
+
+gulp.task('hint',function(){
+  return gulp.src('./src/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter());
+});
+
+gulp.task('test', function () {
+    return gulp.src(testFiles, {read: false})
+           .pipe(mocha({reporter: 'nyan'}))
+           .once('end', function () {
+             process.exit();
+           });
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'bin/www',
+    ext: 'js jade coffee',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+gulp.task('default', [
+  'develop'
+]);
---
+++ test/queryTransalteTest.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Assert Library
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+
+// Library
+var lib = require('../app/scripts/');
+
+describe("Get word and send to google translate to get translate word ",function(){
+		var userId = "111111111",
+				contributeValue = 80,
+				roadId = "1",
+				dateStart = Date.now();
+	it("send word to google translate and would get json",function(done){
+		this.timeout(20000);
+	});
+
+});
