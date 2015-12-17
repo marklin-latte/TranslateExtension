f50a870d10188bd76c6b91d44e83283fd289b103
mark <h091237557@gmail.com>
2015-12-17 21:23:11 +0800

    Fix:fix some page could tranancy

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,7 @@
+/* @flow */
 (function(){
-
 var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-TW&hl=zh-TW&dt=t&dt=bd&dj=1&source=icon&tk=509566|266802&q=";
+
 
 function createDialogUI(){
 
